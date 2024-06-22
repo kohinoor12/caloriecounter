b33a7e46f417757b797cd3c922c59170a36cebc7
kohinoor12 <kohinoorbharti7@gmail.com.com>
2024-06-22 13:52:31 +0530

    push complete

--- caloriecounter/settings.py
+++ caloriecounter/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-((q2zbl6(t_78sufuap(usm^q#21k0#x_0-#t@ld7=n52rulx@
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['.vercel.app','now.sh','127.0.0.1','localhost']
 
 
 # Application definition
@@ -120,10 +120,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = 'static/'
-STATIC_ROOT  = os.path.join(BASE_DIR, 'static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build', 'static')
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'caloriecounter/static/')
 ]
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
